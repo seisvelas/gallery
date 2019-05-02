b33e446d740db8534edf64458773350abd45840d
seisvelas <44826516+seisvelas@users.noreply.github.com>
2019-05-01 19:52:30 -0500

    Update clockAngle.js

--- js/clockAngle.js
+++ js/clockAngle.js
@@ -2,6 +2,6 @@ let hourPos = (hour, min) =>
   5 *  ((hour % 12) + (min / 60))
           
 let clockAngle = (hour, min, sec) => {
-  let angle = Math.abs(sec - hourPos(hour, min))
-  return 6 * Math.min(angle, 360 - angle)
+  let angle = 6 * Math.abs(sec - hourPos(hour, min))
+  return Math.min(angle, 360 - angle)
 }
